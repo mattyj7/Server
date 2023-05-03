bcd5516ce0af074217b8a12267ca9f5d23ad2d5a
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-05-03 13:32:07 -0600

    Added about page and 404 page

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
  
+    let path = '';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
 
 // set header content type
  res.setHeader('Content-Type', 'text/html');
 
  // send an html file
- fs.readFile('./index.html', (err, data) => {
+ fs.readFile(path, (err, data) => {
     if(err) {
         console.log(err);
         res.end();
