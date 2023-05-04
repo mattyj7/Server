90f31b5611ccb9224992a6f7c94a30a2effd117f
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-05-03 20:20:35 -0600

    Installed npm and nodemon to access 3rd party modules

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
  
+    // lodash
+    const num = _.random(0, 20);
+    console.log(num)
+
     let path = '';
     switch (req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
