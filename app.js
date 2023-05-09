7718589cc2beaa33cd572bc1061601dd7dec0a76
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-05-08 20:40:26 -0600

    Added CSS to blog list and created route parameters for specific ids

--- app.js
+++ app.js
@@ -43,6 +43,17 @@ app.post('/blogs', (req, res) => {
         })
 })
 
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id) 
+        .then(result => {
+            res.render('details', { blog: result, title: 'Blog Details'})
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home'});
 });
